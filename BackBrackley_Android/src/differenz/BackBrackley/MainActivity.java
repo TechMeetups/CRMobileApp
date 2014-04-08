5b4c2c13a54fc3701a0ae78af5f07b8e0346c563
Fazil <fazil@differenz.co.in>
2014-04-08 21:01:22 +0530

    Android Version check in

---
+++ BackBrackley_Android/src/differenz/BackBrackley/MainActivity.java
@@ -0,0 +1,19 @@
+
+package differenz.BackBrackley;
+
+import android.os.Bundle;
+
+import org.apache.cordova.*;
+
+
+public class MainActivity extends DroidGap {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/index.html");
+	}
+
+	
+
+}
